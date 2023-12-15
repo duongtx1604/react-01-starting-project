0865b979f5396d125acc0c8a12fecab7d99d7d45
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-12-15 11:40:46 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
+function Header() {
+    const description =
+        reactDescriptions[getRandomInt(reactDescriptions.length)];
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {description} React concepts you will need for almost any app
+                you are going to build!
+            </p>
+        </header>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The core UI building block."
+                            image={componentsImg}
+                        />
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
